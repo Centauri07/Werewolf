daa726236ed8748b83ebe0647ab1ade425865b04
Centauri07 <magnus.centauri07@gmail.com>
2022-08-18 09:53:01 +0800

    added necessary dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,10 +8,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven(url = uri("https://jitpack.io"))
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
+    implementation("com.github.Centauri07:JarbAPI:c971c1df42")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
